273a06cf7a8de48874ceb1f9b60fdaa97b199346
Guandaru-Kabugi <kabugi96@gmail.com>
2024-08-30 14:56:58 +0300

    first push of task 3

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
 from .views import BookList, ListCreateBookView,get_view
-from .views import BookViewSet
+from .views import BookViewSet,login,signup
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'bookviewset',BookViewSet,basename='bookviewset')
@@ -11,4 +12,7 @@ urlpatterns = [
     path('api/books/create/',ListCreateBookView.as_view()),
     path('api/books/',BookList.as_view()),
     path('api/get_view/',get_view),
+    path('login/',login),
+    path('signup/',signup),
+    path('api-token-auth/', obtain_auth_token)
 ]
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
