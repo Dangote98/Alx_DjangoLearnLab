1322db1eb1b8288d21362a78a0e0ab46d8fb75bf
Guandaru-Kabugi <kabugi96@gmail.com>
2024-09-06 12:21:54 +0300

    second commit of task 1

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from .views import CreateView,ListView,DeleteView,UpdateView,DetailView
 urlpatterns = [
-    path('',CreateView.as_view()),
-    path('listview/',ListView.as_view()),
-    path('deleteview/<int:pk>/',DeleteView.as_view()),
-    path('updateview/<int:pk>/',UpdateView.as_view()),
+    path("books/create",CreateView.as_view()),
+    path("books",ListView.as_view()),
+    path("books/delete/<int:pk>/",DeleteView.as_view()),
+    path("books/update/<int:pk>/",UpdateView.as_view()),
     path('detailview/<int:pk>/',DetailView.as_view()),
 ]
