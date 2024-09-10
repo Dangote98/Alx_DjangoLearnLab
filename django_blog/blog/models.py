a2390f74fdd2cf00849fe0ac22a36ff1cf4ff568
Guandaru-Kabugi <kabugi96@gmail.com>
2024-09-10 15:02:58 +0300

    Second push of my new blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,43 +1,44 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser,BaseUserManager
+from django.contrib.auth.models import User
 # Create your models here.
-class UserManager(BaseUserManager):
-    def create_user(self,email,username,password,first_name,last_name):
-        if not email:
-            raise ValueError("Please provide an email")
-        if not username:
-            raise ValueError("Please provide a username")
-        user = self.model(
-            email=self.normalize_email(email),
-            username = username,
-            first_name = first_name,
-            last_name = last_name
-        )
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-    def create_superuser(self,email,username,password,first_name,last_name):
-        user = self.create_user(
-            email= self.normalize_email(email),
-            username=username,
-            first_name=first_name,
-            last_name=last_name,
-            password=password
-        )
-        user.is_staff = True
-        user.is_admin = True
-        user.is_superuser = True
-        user.save(using=self._db)
-        return user
-class User(AbstractUser):
-    email = models.EmailField(unique=True,max_length=155)
-    username = models.CharField(unique=True,max_length=50)
-    first_name = models.CharField(unique=False,max_length=50)
-    last_name = models.CharField(unique=False,max_length=50)
+# class UserManager(BaseUserManager):
+#     def create_user(self,email,username,password,first_name,last_name):
+#         if not email:
+#             raise ValueError("Please provide an email")
+#         if not username:
+#             raise ValueError("Please provide a username")
+#         user = self.model(
+#             email=self.normalize_email(email),
+#             username = username,
+#             first_name = first_name,
+#             last_name = last_name
+#         )
+#         user.set_password(password)
+#         user.save(using=self._db)
+#         return user
+#     def create_superuser(self,email,username,password,first_name,last_name):
+#         user = self.create_user(
+#             email= self.normalize_email(email),
+#             username=username,
+#             first_name=first_name,
+#             last_name=last_name,
+#             password=password
+#         )
+#         user.is_staff = True
+#         user.is_admin = True
+#         user.is_superuser = True
+#         user.save(using=self._db)
+#         return user
+# class User(AbstractUser):
+#     email = models.EmailField(unique=True,max_length=155)
+#     username = models.CharField(unique=True,max_length=50)
+#     first_name = models.CharField(unique=False,max_length=50)
+#     last_name = models.CharField(unique=False,max_length=50)
     
-    objects = UserManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+#     objects = UserManager()
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
 
 class Post(models.Model):
     title = models.CharField(max_length=200, null=False, unique=True)
--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -126,7 +126,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'blog.User'
+# AUTH_USER_MODEL = 'blog.User'
 
 STATIC_URL = '/static/'
 
