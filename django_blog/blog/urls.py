41c1f8cf85137d624975af919cf9050c2805211b
Guandaru-Kabugi <kabugi96@gmail.com>
2024-09-11 11:16:54 +0300

    second push of task 2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('login/',LoginUser.as_view(),name='login'),
     path('logout/',LogoutUser.as_view(next_page='/'),name='logout'),
     path('profile/',profileview,name='profile'),
-    path('posts/new/',CreatePostView.as_view(), name='create_post'),
+    path('post/new/',CreatePostView.as_view(), name='create_post'),
     path('posts/',ListPostView.as_view(),name='list_posts'),
-    path('posts/<int:pk>/edit/',UpdatePostView.as_view(), name='edit_post'),
-    path('posts/<int:pk>/delete/',DeletePostView.as_view(),name='delete_post'),
-    path('posts/<int:pk>/',DetailPostView.as_view(),name='detail_post'),
+    path('post/<int:pk>/update/',UpdatePostView.as_view(), name='edit_post'),
+    path('post/<int:pk>/delete/',DeletePostView.as_view(),name='delete_post'),
+    path('post/<int:pk>/',DetailPostView.as_view(),name='detail_post'),
 ]
